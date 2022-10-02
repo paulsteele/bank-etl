c6d4c320a8a01c9f285fe1083cf0177e9a641ecc
Paul Steele <paul@paul-steele.com>
2022-10-01 20:19:38 -0400

    cleanup nuke

--- build/Build.cs
+++ build/Build.cs
@@ -1,47 +1,31 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
-using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 
-[CheckBuildProjectConfigurations]
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")] readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+	[Solution] readonly Solution Solution;
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	Target Clean => _ => _
+		.Before(Restore)
+		.Executes(() =>
+		{
+			
+		});
 
-    [Solution] readonly Solution Solution;
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+			
+		});
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+	Target Compile => _ => _
+		.DependsOn(Restore)
+		.Executes(() =>
+		{
+			
+		});
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
-
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
-
+    public static int Main() => Execute<Build>(x => x.Compile);
 }
